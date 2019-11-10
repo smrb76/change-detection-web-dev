c445e28840da792940ace6d171cfdb0179bc65c4
smrb76 <smrb1376@gmail.com>
2019-11-10 03:25:17 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { ListGenerator, EmployeeData } from './tree-generator.service';
+import { Component, OnInit } from '@angular/core';
+import { Names } from './names';
+
+const NumRange: [number, number] = [23, 28];
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'app-root',
+  template: `
+    <app-employee-list
+      [data]="salesList"
+      department="Sales"
+      (add)="add(salesList, $event)"
+      (remove)="remove(salesList, $event)"
+    ></app-employee-list>
+
+    <app-employee-list
+      [data]="rndList"
+      department="R&D"
+      (add)="add(rndList, $event)"
+      (remove)="remove(rndList, $event)"
+    ></app-employee-list>
+  `,
+  styleUrls: ['app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  salesList: EmployeeData[];
+  rndList: EmployeeData[];
+  label: string;
+
+  constructor(private generator: ListGenerator) {}
+
+  ngOnInit() {
+    this.salesList = this.generator.generate(Names, NumRange, 100);
+    this.rndList = this.generator.generate(Names, NumRange, 100);
+  }
+
+  add(list: EmployeeData[], name: string) {
+    list.unshift({ label: name, num: this.generator.generateNumber(NumRange) });
+  }
+
+  remove(list: EmployeeData[], node: EmployeeData) {
+    list.splice(list.indexOf(node), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { EmployeeListModule } from './employee-list/employee-list.module';
+import { ListGenerator } from './tree-generator.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, EmployeeListModule, BrowserAnimationsModule],
+  providers: [ListGenerator],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
